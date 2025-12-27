0a457a2c4114dceef6feae73e29b0bc3a9d2a20a
Sean <127526527+Sean-Zeo@users.noreply.github.com>
2025-12-27 23:35:17 +0400

    Update PurrNetNetworkManagerHUD.cs

--- MultiRoomManagerForPurrNet/PurrNetNetworkManagerHUD.cs
+++ MultiRoomManagerForPurrNet/PurrNetNetworkManagerHUD.cs
@@ -32,7 +32,10 @@ public class PurrNetNetworkManagerHUD : MonoBehaviour
 #endif
             GUILayout.BeginHorizontal();
             if (GUILayout.Button("Client"))
+            {
+                SetTransportSettings();
                 InstanceHandler.NetworkManager.StartClient();
+            }
 
             addressInput = GUILayout.TextField(addressInput);
             portInput = GUILayout.TextField(portInput);
